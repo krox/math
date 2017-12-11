e8aff564a86e52f9eb339df3ef524db06c3c32a8
Simon Bürger <simon.buerger@rwth-aachen.de>
2017-12-11 17:37:08 +0100

    adaptive Gauss-Kronrod integration with 15/31 points

--- math/integration.d
+++ math/integration.d
@@ -4,10 +4,11 @@ module math.integration;
  * Numerical integration based on quadrature schemes such as Gauss-Legendre.
  */
 
-private import std.exception;
-private import std.math;
-private import std.format;
-private import math.solve;
+import std.exception;
+import std.math;
+import std.format;
+import jive.priorityqueue;
+import math.solve;
 
 //////////////////////////////////////////////////////////////////////
 /// One-Dimensional integration
@@ -28,6 +29,73 @@ double integrateGL(F)(F f, double a, double b, int n)
 	for(size_t i = n%2; i < q.x.length; ++i)
 		sum += q.w[i] * (f(mid - halfSize * q.x[i]) + f(mid + halfSize * q.x[i]));
 	return sum * halfSize;
+}
+
+/**
+ * Adaptive Gauss-Kronrod qudrature using 15/31 points.
+ */
+double integrateGK(F)(F f, double a, double b)
+{
+	static struct Region
+	{
+		double a, b;
+		double val, err;
+		this(F f, double a, double b)
+		{
+			auto est = integrateKronrod(f, a, b, GK15);
+			this.a = a;
+			this.b = b;
+			this.val = est[1];
+			this.err = abs(est[1]-est[0]);
+		}
+	}
+
+	PriorityQueue!(Region, "a.err > b.err") q;
+
+	auto reg = Region(f, a, b);
+	q.pushBack(reg);
+	double val = reg.val;
+	double err = reg.err;
+	while(abs(err/val) > 1.0e-10)
+	{
+		if(q.length >= 100)
+			throw new Exception("Gauss-Kronrod adaptive integral did not converge.");
+
+		reg = q.pop;
+		auto regLeft = Region(f, reg.a, 0.5*(reg.a+reg.b));
+		auto regRight = Region(f, 0.5*(reg.a+reg.b), reg.b);
+		val += regLeft.val + regRight.val - reg.val;
+		err += regLeft.err + regRight.err - reg.err;
+		q.push(regLeft);
+		q.push(regRight);
+	}
+	return val;
+}
+
+private double[2] integrateKronrod(F)(F f, double a, double b, GaussKronrodQuadrature!double q)
+{
+	double mid = (a+b)/2;
+	double half = (b-a)/2;
+
+	assert(q.x[0] == 0); // only odd GK rules are supported
+	double f0 = f(mid);
+	double sumG = q.wG[0]*f0;
+	double sumK = q.wK[0]*f0;
+	for(size_t i = 1; i < q.wG.length; ++i)
+	{
+		f0 = f(mid - half * q.x[i]) + f(mid + half * q.x[i]);
+		sumG += q.wG[i] * f0;
+		sumK += q.wK[i] * f0;
+	}
+	for(size_t i = q.wG.length; i < q.wK.length; ++i)
+	{
+		f0 = f(mid - half * q.x[i]) + f(mid + half * q.x[i]);
+		sumK += q.wK[i] * f0;
+	}
+
+	sumG *= half;
+	sumK *= half;
+	return [sumG, sumK];
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -230,3 +298,60 @@ unittest
 		assert(abs(w20[i] - cast(double)legendreRoots!double(20).w[i]) < 1e-15);
 	}
 }
+
+struct GaussKronrodQuadrature(T)
+{
+	// wG.length < wK.length = x.length
+	immutable(T)[] x;
+	immutable(T)[] wG;
+	immutable(T)[] wK;
+}
+
+// values taken from https://www.advanpix.com/2011/11/07/gauss-kronrod-quadrature-nodes-weights/
+private static GK15 = GaussKronrodQuadrature!double(
+	[ // Gauss nodes
+	0.000000000000000000000000000000000e+00,
+	2.011940939974345223006283033945962e-01,
+	3.941513470775633698972073709810455e-01,
+	5.709721726085388475372267372539106e-01,
+	7.244177313601700474161860546139380e-01,
+	8.482065834104272162006483207742169e-01,
+	9.372733924007059043077589477102095e-01,
+	9.879925180204854284895657185866126e-01,
+	// additional Kronrod nodes
+	1.011420669187174990270742314473923e-01,
+	2.991800071531688121667800242663890e-01,
+	4.850818636402396806936557402323506e-01,
+	6.509967412974169705337358953132747e-01,
+	7.904185014424659329676492948179473e-01,
+	8.972645323440819008825096564544959e-01,
+	9.677390756791391342573479787843372e-01,
+	9.980022986933970602851728401522712e-01,
+	],[ // Gauss weights
+	2.025782419255612728806201999675193e-01,
+	1.984314853271115764561183264438393e-01,
+	1.861610000155622110268005618664228e-01,
+	1.662692058169939335532008604812088e-01,
+	1.395706779261543144478047945110283e-01,
+	1.071592204671719350118695466858693e-01,
+	7.036604748810812470926741645066734e-02,
+	3.075324199611726835462839357720442e-02,
+	],[ // Kronrod weights
+	1.013300070147915490173747927674925e-01,
+	9.917359872179195933239317348460313e-02,
+	9.312659817082532122548687274734572e-02,
+	8.308050282313302103828924728610379e-02,
+	6.985412131872825870952007709914748e-02,
+	5.348152469092808726534314723943030e-02,
+	3.534636079137584622203794847836005e-02,
+	1.500794732931612253837476307580727e-02,
+
+	1.007698455238755950449466626175697e-01,
+	9.664272698362367850517990762758934e-02,
+	8.856444305621177064727544369377430e-02,
+	7.684968075772037889443277748265901e-02,
+	6.200956780067064028513923096080293e-02,
+	4.458975132476487660822729937327969e-02,
+	2.546084732671532018687400101965336e-02,
+	5.377479872923348987792051430127650e-03,
+	]);
